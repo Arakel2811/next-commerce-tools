3b1166f8dc22024ca1519f85d235114c0ee20b61
Arakel2811 <arakel.galeyan@gmail.com>
2023-10-05 13:01:14 +0400

    Initial commit & test pages.

---
+++ next-commerce-tools/src/app/product/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Product(){
+    return (
+        <>
+            <div>Product page</div>
+            <Link href={'/'} > Test link </Link>
+        </>
+    )
+}
